b1ba7098561f16f2f4c8ee2a79f4919a0f8d7ce2
kreis <lkreisma@htl-steyr.ac.at>
2026-01-04 15:38:40 +0100

    first gridPane + start of login style

--- src/main/java/htl/steyr/minesweeper_lkreisma/HelloController.java
+++ src/main/java/htl/steyr/minesweeper_lkreisma/HelloController.java
@@ -1,14 +1,47 @@
 package htl.steyr.minesweeper_lkreisma;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void StartingGameClicked() {
+        Stage stage = new Stage();
+        AnchorPane playroot = new AnchorPane();
+        Scene playscene = new Scene(playroot, 500, 500);
+
+
+        //making the gridpane
+        playroot.getChildren().add(createGrid(10));
+
+
+
+        stage.setScene(playscene);
+        stage.show();
+
+    }
+
+
+    //method for creating a new GridPane
+
+    public GridPane createGrid(int rowcolumn){
+        GridPane gridpane = new GridPane();
+
+        for(int b=0; b<rowcolumn;++b){
+            gridpane.addRow(b);
+
+            for(int i=0; i<rowcolumn; ++i){
+                Button button = new Button(i+":"+b);
+                gridpane.addColumn(i, button);
+            }
+        }
+
+        return gridpane;
     }
 }
