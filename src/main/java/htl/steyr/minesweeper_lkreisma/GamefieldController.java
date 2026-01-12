345d8510a3c0120b3f20c9b95ca1ff26318e4428
kreis <lkreisma@htl-steyr.ac.at>
2026-01-12 14:12:47 +0100

    nothing works - trying to fix

--- src/main/java/htl/steyr/minesweeper_lkreisma/GamefieldController.java
+++ src/main/java/htl/steyr/minesweeper_lkreisma/GamefieldController.java
@@ -1,54 +1,60 @@
 package htl.steyr.minesweeper_lkreisma;
 
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class GamefieldController {
 
-    Label timer = new Label();
 
 
-    public static void newGameField(ActionEvent actionEvent, ChoiceBox<String> difficulty){
+
+
+    public static void newGameField(ActionEvent actionEvent, ChoiceBox<String> difficulty) {
         System.out.println(difficulty.getValue());
+        ((Stage) ((Node) actionEvent.getSource()).getScene().getWindow()).close();
 
         Stage stage = new Stage();
         AnchorPane playroot = new AnchorPane();
         Scene playscene = new Scene(playroot);
+
+        //Start-Window will be closed
         stage.setScene(playscene);
 
 
         //making the gridpane
-        switch(difficulty.getValue()){
-            case "Anfänger" -> playroot.getChildren().add(createGrid(8, 8));
-            case null -> playroot.getChildren().add(createGrid(8, 8));
-            case "Forteschrittener" -> playroot.getChildren().add(createGrid(16, 16));
-            case "Profi" -> playroot.getChildren().add(createGrid(16, 30));
+        switch (difficulty.getValue()) {
+            case "Anfänger" -> playroot.getChildren().add(createGrid(8, 8, 10));
+            case null -> playroot.getChildren().add(createGrid(8, 8, 10));
+            case "Forteschrittener" -> playroot.getChildren().add(createGrid(16, 16, 40));
+            case "Profi" -> playroot.getChildren().add(createGrid(16, 30, 99));
             default -> throw new IllegalStateException("Unexpected value: " + difficulty.getValue());
         }
 
 
         stage.show();
 
-        //Start-Window will be closed
-        ((Stage) ((Node) actionEvent.getSource()).getScene().getWindow()).close();
+
     }
 
 
     //method for creating a new GridPane
-    public static GridPane createGrid(int row, int column) {
+    public static GridPane createGrid(int row, int column, int bombs) {
         GridPane gridpane = new GridPane();
-
         for (int b = 0; b < row; ++b) {
             gridpane.addRow(b);
 
             for (int i = 0; i < column; ++i) {
+                int randomnumber = ThreadLocalRandom.current().nextInt(bombs);
+                System.out.println(randomnumber);
                 Button button = new Button(i + ":" + b);
                 //button.setBackground(null);
                 button.setPrefSize(50, 50);
@@ -56,8 +62,17 @@ public class GamefieldController {
             }
         }
 
+
+        while(bombs > 0){
+            System.out.println(gridpane.getChildren().get(2));
+
+        }
+
         gridpane.setBackground(null);
 
         return gridpane;
     }
+
+
+
 }
--- src/main/java/htl/steyr/minesweeper_lkreisma/HelloController.java
+++ src/main/java/htl/steyr/minesweeper_lkreisma/HelloController.java
@@ -1,11 +1,14 @@
 package htl.steyr.minesweeper_lkreisma;
 
+import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+
+import java.beans.JavaBean;
 
 
 public class HelloController {
@@ -18,14 +21,15 @@ public class HelloController {
         return difficultyChoiceBox;
     }
 
+
     public void initialize() {
         ObservableList<String> difficulties = FXCollections.observableArrayList("Anfänger", "Forteschrittener", "Profi");
         difficultyChoiceBox.setItems(difficulties);
     }
 
     @FXML
-    protected void StartingGameClicked(ActionEvent actionEvent) {
-        GamefieldController.newGameField(actionEvent, difficultyChoiceBox);
+    public void StartingGameClicked(ActionEvent actionEvent) {
+        GamefieldController.newGameField(actionEvent, getChosenDifficulty());
     }
 
     public void QuitGame(ActionEvent actionEvent) {
